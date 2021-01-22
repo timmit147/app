6e29fdf892997e4dd2550ab75ccad2fd1fbe6ba6
timmit147 <timmeeuwsen@hotmail.nl>
2021-01-22 14:06:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 var save = document.getElementById("save");
 var clear = document.getElementById("clear");
-var data;
+var data = window.localStorage.getItem('data');
+var i = 1;
+var goal =  data.split(',');
 
+
+// remove goal
+function removefuntion(number) {
+goal = goal.toString().replace(","+goal[number], ''); 
+window.localStorage.setItem('data', goal);
+window.location.href = "index.html";
+}
+
+
+
+// clear all data
 if(clear){
 	clear.addEventListener("click", clearfuntion);
 }
-
-var testObject = { 'one': 1, 'two': 2, 'three': 3 };
-console.log(testObject['two']);
 
 function clearfuntion() {
 localStorage.clear();
 }
 
 
+// set goal listerner
 if(save){
 	save.addEventListener("click", myFunction);
 }
 
 function myFunction() {
-	data = window.localStorage.getItem('data');
 	data = data + "," + document.getElementById("titel").value + "^" + document.getElementById("goal").value  + "^" + document.getElementById("amount").value;
 	window.localStorage.setItem('data', data);
 	window.location.href = "index.html";
 }
 
 
-
+// load data
 if(document.getElementById("home")){
 	window.onload = function() {
 		if(window.localStorage){
-			var i = 1;
 			while (i < 100) {
-			  const goal =  window.localStorage.getItem('data').split(','); 
-			  document.getElementById('log').innerHTML += "<button></button> <p>" + goal[i].split('^')[0] + "</p>" + "<div class='bar'><div style='width: calc(100% / "+goal[i].split('^')[1]+" * "+goal[i].split('^')[2]+");' class='progress'></div></div>";
-			  goal[i].split('^')[1]
+			  document.getElementById('log').innerHTML += "<button onclick='removefuntion("+i+")'>Remove</button> <button onclick='subfuntion("+i+")'>sub</button> <button onclick='addfuntion("+i+")'>Add</button> <p>" + goal[i].split('^')[0] + "</p> <p>" + goal[i].split('^')[1] + "</p><p>" + goal[i].split('^')[2] + "</p>" + "<div class='bar'><div style='width: calc(100% / "+goal[i].split('^')[1]+" * "+goal[i].split('^')[2]+");' class='progress'></div></div>";
 			  	i++;
 
 			  	if (goal[i] == undefined){
@@ -46,3 +53,22 @@ if(document.getElementById("home")){
 	}
 }
 
+// add value
+function addfuntion(number) {
+var y = goal[number].split('^')[2];
+  var z = 1;
+  var x = +y + +z;
+goal = goal.toString().replace(y, x ); 
+window.localStorage.setItem('data', goal);
+window.location.href = "index.html";
+}
+
+// remove value
+function subfuntion(number) {
+var y = goal[number].split('^')[2];
+  var z = 1;
+  var x = +y - +z;
+goal = goal.toString().replace(y, x ); 
+window.localStorage.setItem('data', goal);
+window.location.href = "index.html";
+}
